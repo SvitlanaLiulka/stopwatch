44c67b7ee35d3570a5600d7d7856416098263208
Svitlana <lyulkalyulka@gmail.com>
2022-01-09 21:01:22 +0200

    fix time

--- src/componens/time/time.js
+++ src/componens/time/time.js
@@ -4,14 +4,14 @@ import './time.css';
 export default function Time(props) {
     return (
       <div className="timer">
+         <span className="timer-min">
+          {("0" + Math.floor((props.time / (1000 * 60 * 60)) % 24)).slice(-2)}:
+          </span>
         <span className="timer-min">
           {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
         </span>
         <span className="timer-min">
-          {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}:
-        </span>
-        <span className="timer-min">
-          {("0" + ((props.time / 10) % 100)).slice(-2)}
+          {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}
         </span>
       </div>
     );
